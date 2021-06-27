671608c35cc5f1a14bd1f7377856430226ecb91b
ARIHMA KRISHNAN A <85427484+ArishmaKrishnanA@users.noreply.github.com>
2021-06-27 20:45:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p1>A Basic Web dev React Js Bootcamp.</p1>
+      <p2> This is an amazing bootcamp taken by Shaurya Sinha.We covered everthing from scratch including Javascript React,js,HTML.</p2>
+    </div>
+  );
+}
+
+export default Info;
